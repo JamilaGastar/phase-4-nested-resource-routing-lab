63d470c86cb2d131b53f9d5b805690afe0bc6ddc
jamilagastar <jamilagastar@gmail.com>
2022-11-06 15:26:31 +1100

    completed lab

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,35 @@
 class ItemsController < ApplicationController
+  wrap_parameters format: []
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
+    render json: items, except: [:updated_at, :created_at, :user], include: :user
+  end
+  
+  def show
+    item = Item.find(params[:id])
+    render json: item, except: [:updated_at, :created_at, :user], include: :user
+  end
+
+  def create
+    item = Item.create(item_params)
+    render json: item, except: [:updated_at, :created_at, :user], status: :created
+  end
+
+  private
+
+  def render_not_found_response
+    render json: { error: "User not found" }, status: :not_found
+  end
+
+  def item_params
+    params.permit(:item, :name, :description, :price, :user_id)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
+  wrap_parameters format: []
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def show
-    user = User.find_by(id: params[:id])
-    render json: user, include: :items
+    user = User.find(params[:id])
+    render json: user, except: [:created_at, :updated_at], include: :items 
+  end
+
+  private
+
+  def render_not_found_response
+    render json: { error: "User not found" }, status: :not_found
   end
 
 end
